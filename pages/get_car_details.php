adffe17964c375b99082200d616e624589974fe3
viethoang1508 <15082004qwerty@gmail.com>
2025-03-20 19:33:11 +0700

    update car-menu

--- pages/get_car_details.php
+++ pages/get_car_details.php
@@ -80,11 +80,11 @@ if (isset($_GET['name'])) {
             <tr><th>Exterior</th><td><?= nl2br(htmlspecialchars($car['exterior'])) ?></td></tr>
         </table>
         <div class="text-center mt-3">
-    <a href="../pages/contact.php" class="contact-button">LIÊN HỆ</a>
+    <a href="../pages/contact.php" class="contact-button">CONTACT</a>
       </div>
         <?php
     } else {
-        echo "<p class='text-danger'>Không tìm thấy thông tin xe.</p>";
+        echo "<p class='text-danger'>Cannot find information.</p>";
     }
 }
 ?>
