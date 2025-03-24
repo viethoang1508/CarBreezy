785a26b2e6b57e0caac3592f2a6678a2bd13e71d
Chuong Pham <hoangchuong869@gmail.com>
2025-03-24 22:40:31 +0700

    update

--- includes/connect.php
+++ includes/connect.php
@@ -1,11 +1,7 @@
 <?php
     $host = "localhost:3306";
     $user = "root";
-<<<<<<< HEAD
     $password = "200426";
-=======
-    $password = "123456789";
->>>>>>> bcf8befb0248a995ba9a8c9172323039de014a14
     $database = "car_breezy";
     $mysqli = new mysqli($host, $user, $password, $database);
 
